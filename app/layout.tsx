868f3c17d12088a953fc855704551e87b1fc3295
Uni0305 <git@uni0305.me>
2023-08-04 03:27:32 +0900

    Feat: Add custom font 'Pretendard Variable'

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.8/variable/pretendardvariable.css"
+        />
+      </head>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,25 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: [
+        "Pretendard Variable",
+        "Pretendard",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "system-ui",
+        "Roboto",
+        "Helvetica Neue",
+        "Segoe UI",
+        "Apple SD Gothic Neo",
+        "Noto Sans KR",
+        "Malgun Gothic",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "sans-serif",
+      ],
+    },
   },
   plugins: [],
 };
